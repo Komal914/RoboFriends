cbcaa8bc25e9dbc14fe9cd8c85c95382b6453089
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2022-11-20 15:38:31 -0500

    search bar rerenders the robot cards

--- Desktop/robofriends/src/App.js
+++ Desktop/robofriends/src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import CardList from "./CardList";
 import { robots } from "./robots";
+import SearchBox from "./SearchBox";
 
-const App = () => {
-  return <CardList robots={robots} />;
-};
+class App extends Component {
+  constructor() {
+    super();
+    //state can affect our app's description from parent to child (App -> components )
+    this.state = {
+      robots: robots,
+      SearchBox: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ SearchBox: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.SearchBox.toLowerCase());
+    });
+    return (
+      <div>
+        <h1>Robot Friends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
