219a462c2b739a9e8611c1d128537f74f6c24e79
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2023-01-03 12:54:59 -0500

    going back to class component for app from functional

--- Desktop/robofriends/src/containers/App.js
+++ Desktop/robofriends/src/containers/App.js
@@ -1,76 +1,98 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components /CardList";
 import SearchBox from "../components /SearchBox";
 import Scroll from "../components /Scroll.js";
 import ErrorBoundry from "../components /ErrorBoundry";
+import { setSearchField } from "../actions";
 import "./App.css";
+import { render } from "react-dom";
 
-function App() {
+//redux: getting states
+const mapStateToProps = (state) => {
+  return {
+    //calling my reducer to set state
+    searchField: state.searchField,
+  };
+};
+
+//redux: sends dispatch to reducer to trigger the action for state
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  };
+};
+class App extends Component {
   //REACT HOOKS: STATE: useState returns to use the state
-  const [robots, setRobots] = useState([]);
-  const [searchBox, setSearchBox] = useState("");
+  // const [robots, setRobots] = useState([]);
+  // const [searchBox, setSearchBox] = useState("");
 
-  // constructor() {
-  //   super();
-  //   //state can affect our app's description from parent to child (App -> components )
-  //   this.state = {
-  //     robots: [],
-  //     SearchBox: "",
-  //   };
-  // }
+  constructor() {
+    super();
+    //state can affect our app's description from parent to child (App -> components )
+    this.state = {
+      robots: [],
+      SearchBox: "",
+    };
+  }
 
   //REACT HOOKS: EFFECT
   //effect helps with lifecycle functions like component did mount
   //ran everytime app is rendered
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        return response.json();
-      })
-      .then((users) => {
-        setRobots(users); //we change the state of robots,
-        //so the use effects func will re-render the app the reflect the changes
-        // creates a loop
-        //set state -> render -> set state -< render
-      });
-  }, []); //we only use effect when value changes (second param) to avoid looping of re-renders
-  // we give an empty array cuz the value will not change
-
-  //mounting functions, part of the react functions
-  //not am arrow function
-  // componentDidMount() {
+  // useEffect(() => {
   //   fetch("https://jsonplaceholder.typicode.com/users")
   //     .then((response) => {
   //       return response.json();
   //     })
   //     .then((users) => {
-  //       this.setState({ robots: users });
+  //       setRobots(users); //we change the state of robots,
+  //       //so the use effects func will re-render the app the reflect the changes
+  //       // creates a loop
+  //       //set state -> render -> set state -< render
   //     });
-  // }
+  // }, []); //we only use effect when value changes (second param) to avoid looping of re-renders
+  // we give an empty array cuz the value will not change
 
-  const onSearchChange = (event) => {
-    //using state function to set state
-    setSearchBox(event.target.value);
-  };
+  //mounting functions, part of the react functions
+  //not am arrow function
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
 
-  const filteredRobots = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchBox.toLowerCase());
-  });
+  // const onSearchChange = (event) => {
+  //   //using state function to set state
+  //   setSearchBox(event.target.value);
+  // };
 
-  return !robots.length ? (
-    <h1>Loading...</h1>
-  ) : (
-    <div className="tc">
-      <h1 className="f1">Robot Friends</h1>
-      <SearchBox searchChange={onSearchChange} />
-      <Scroll>
-        <ErrorBoundry>
-          <CardList robots={filteredRobots} />
-        </ErrorBoundry>
-      </Scroll>
-    </div>
-  );
+  render() {
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
+
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">Robot Friends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+  }
 }
 
-export default App;
+//higher order function connect returns another function which will take in App as param
+export default connect(mapStateToProps, mapDispatchToProps)(App); //connecting to redux, passes states as props to APP COMPONENT
--- Desktop/robofriends/src/reducers.js
+++ Desktop/robofriends/src/reducers.js
@@ -3,8 +3,10 @@ const initialState = {
   searchField: "",
 };
 
+//reducer function for actions per state
 export const searchRobots = (state = initialState, action = {}) => {
   switch (action.type) {
+    //set search field state
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, { searchField: action.payload });
     default:
